448208bc7cc86a86cdf683ec08d5e35781271d52
Lawrence Lee <t-lale@microsoft.com>
2019-06-05 22:46:54 +0000

    [build][rules][build_templates]: add network namespace/multi-instance container support
    [build_templates]:
    	*add network namespace creation/linking to docker_image_ctl.j2
    	*add support for multi-instance containers to docker_image_ctl.j2
    	*add network namespace service to sonic_debian_extension.j2
    
    [rules]:
    	*add rule for creating network namespace docker
    	*removes "--net=host" from DOCKER_RUN_OPTS in all other docker rules (it now lives in docker_image_ctl.j2)
    
    [build]:
    	*update slave.mk to support building template service files
    
    Signed-off-by: Lawrence Lee <t-lale@microsoft.com>

--- rules/docker-fpm-frr.mk
+++ rules/docker-fpm-frr.mk
@@ -7,7 +7,7 @@ $(DOCKER_FPM_FRR)_LOAD_DOCKERS += $(DOCKER_CONFIG_ENGINE_STRETCH)
 SONIC_DOCKER_IMAGES += $(DOCKER_FPM_FRR)
 
 $(DOCKER_FPM_FRR)_CONTAINER_NAME = bgp
-$(DOCKER_FPM_FRR)_RUN_OPT += --net=host --privileged -t
+$(DOCKER_FPM_FRR)_RUN_OPT += --privileged -t
 $(DOCKER_FPM_FRR)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
 $(DOCKER_FPM_FRR)_RUN_OPT += -v /etc/sonic/frr:/etc/frr:rw
 
