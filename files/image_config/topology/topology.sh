cc0a535ed7f3d0f90f052ec4b279428aa685acee
Lawrence Lee <t-lale@microsoft.com>
2019-06-14 14:06:13 -0700

    [files]: add topology service
    * add script to configure virtual switch internal topology
    * create service to manage topology
    * install topology service in sonic_debian_extension
    
    Signed-off-by: Lawrence Lee <t-lale@microsoft.com>

---
+++ files/image_config/topology/topology.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+start () {
+	# Move external links into assigned frontend namespaces
+	# eth0  - eth15: namespace0
+	# eth16 - eth31: namespace1
+	# eth32 - eth47: namespace2
+	# eth48 - eth63: namespace3
+	for NS in `seq 0 3`; do
+		for NUM in `seq 1 16`; do
+			ORIG="eth$((16*$NS+$NUM-1))"
+			TEMP="ethTemp999"
+			ip link set dev $ORIG down
+			ip link set dev $ORIG name $TEMP # rename to prevent conflicts before renaming in new namespace
+			ip link set dev $TEMP netns namespace$NS
+			sudo ip netns exec namespace$NS ip link set $TEMP name eth$NUM # rename to final interface name
+			sudo ip netns exec namespace$NS ip link set eth$NUM up 
+		done
+	done
+
+	# Connect all backend namespaces to frontend namespaces
+	for BACKEND in `seq 4 5`; do
+		for FRONTEND in `seq 0 3`; do
+			for LINK in `seq 1 8`; do
+				BACK_NAME="eth$((8*$FRONTEND+$LINK))"
+				FRONT_OFFSET="$(($BACKEND-2))" # links to namespace4 will be links 17-24 in all frontend namespaces
+				FRONT_NAME="eth$((8*$FRONT_OFFSET+$LINK))" 
+				TEMP_BACK="ethBack999"
+				TEMP_FRONT="ethFront999"
+				
+				ip link add $TEMP_BACK type veth peer name $TEMP_FRONT # temporary name to prevent conflicts between interfaces
+				ip link set dev $TEMP_BACK netns namespace$BACKEND
+				ip link set dev $TEMP_FRONT netns namespace$FRONTEND 
+	
+				sudo ip netns exec namespace$BACKEND ip link set $TEMP_BACK name $BACK_NAME
+				sudo ip netns exec namespace$FRONTEND ip link set $TEMP_FRONT name $FRONT_NAME
+
+				sudo ip netns exec namespace$BACKEND ip link set $BACK_NAME up
+				sudo ip netns exec namespace$FRONTEND ip link set $FRONT_NAME up
+			done
+		done
+	done
+}
+
+stop() {
+	for NS in `seq 0 3`; do
+		for NUM in `seq 1 16`; do
+			TEMP="eth999"
+			NAME="eth$((16*$NS+$NUM-1))"
+			sudo ip netns exec namespace$NS ip link set dev eth$NUM down
+			sudo ip netns exec namespace$NS ip link set dev eth$NUM name $TEMP
+			sudo ip netns exec namespace$NS ip link set dev $TEMP netns 1
+			ip link set dev $TEMP name $NAME
+			ip link set dev $NAME up
+		done
+	done
+
+	for NS in `seq 4 5`; do
+		for NUM in `seq 1 32`; do
+			sudo ip netns exec namespace$NS ip link set dev eth$NUM down
+			sudo ip netns exec namespace$NS ip link delete dev eth$NUM
+		done
+	done
+}
+
+case "$1" in
+	start|stop)
+		$1
+		;;
+	*)
+		echo "Usage: $0 {start|stop}"
+		;;
+esac
+
