89ca96fb93ccd26fbd69d9f9ffb8ce942533ce63
Lawrence Lee <t-lale@microsoft.com>
2019-06-18 13:46:28 -0700

    [files]: change redis socket mount
    * move each redis socket into its own directory on base image
    * mount each base image redis directory at /var/run/redis inside containers for compatibility
    
    Signed-off-by: Lawrence Lee <t-lale@microsoft.com>

--- files/image_config/rsyslog/rsyslog-config.sh
+++ files/image_config/rsyslog/rsyslog-config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sonic-cfggen -s /var/run/redis/redis$1.sock -d -t /usr/share/sonic/templates/rsyslog.conf.j2 >/etc/rsyslog$1.conf
+sonic-cfggen -s /var/run/redis$1/redis.sock -d -t /usr/share/sonic/templates/rsyslog.conf.j2 >/etc/rsyslog$1.conf
 systemctl restart rsyslog
