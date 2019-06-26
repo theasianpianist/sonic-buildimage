f74090c257fc95eb4fdb491c1219702ce2404ec7
Lawrence Lee <t-lale@microsoft.com>
2019-06-26 16:59:39 -0700

    [interfaces-config]: enable for network namespace use
    * assign addresses from config_db to loopback interface in network namespaces if present
    
    Signed-off-by: Lawrence Lee <t-lale@microsoft.com>

--- files/image_config/interfaces/interfaces-config.sh
+++ files/image_config/interfaces/interfaces-config.sh
@@ -2,7 +2,16 @@
 
 ifdown --force eth0
 
-sonic-cfggen -d -t /usr/share/sonic/templates/interfaces.j2 > /etc/network/interfaces
+if [ -z "$(ip netns)"]; then
+	sonic-cfggen -d -t /usr/share/sonic/templates/interfaces.j2 > /etc/network/interfaces 
+else
+	for NS in `seq 0 5`; do
+		INTF=`redis-cli $NS -n 4 KEYS 'LOOPBACK_INTERFACE|Loopback0|*'`
+		IFS='|' read -ra ADDR_ARRAY <<< "$INTF"
+		ADDR=${ADDR_ARRAY[2]}	
+		namespace $NS ip addr add $ADDR dev lo
+	done
+fi
 
 [ -f /var/run/dhclient.eth0.pid ] && kill `cat /var/run/dhclient.eth0.pid` && rm -f /var/run/dhclient.eth0.pid
 
